e0ce5da0dde7093670dcaf1ff5cdfe18aac42741
RAMYFA5RY <eng.ramy.fakhry55@gmail.com>
2023-01-23 07:12:59 -0500

    Removed some code

--- pythonTesting/pythonTesting/main.py
+++ pythonTesting/pythonTesting/main.py
@@ -2,28 +2,9 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-import math
-import sys
-from os import rename
-
 import requests
 
-
-name = input("Your name? ")
-print("Hello ", name)
-print(sys.version)
-print(sys.executable)
-
-
-def print_hi(name):
-    test = "test"
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to runkori
-#  the script.
-if __name__ == "__main__":
-    print_hi("PyCharm")
 r = requests.get("https://coreyms.com")
+
 print(r.status_code)
+print(r.ok)
