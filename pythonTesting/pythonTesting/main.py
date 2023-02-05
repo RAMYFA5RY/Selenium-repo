832d602de2a1bf3bd6aa1d49e5a5d0364a6ba673
RAMYFA5RY <eng.ramy.fakhry55@gmail.com>
2023-02-05 16:42:00 -0500

    Saving Chnages

--- pythonTesting/pythonTesting/main.py
+++ pythonTesting/pythonTesting/main.py
@@ -3,6 +3,11 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import requests
+import pandas as pd
+
+df = pd.read_excel(
+    "C:\\Users\Ramy\\Documents\\VS Code Files\\Python Files\\2- Automating using Selenium\\pythonTesting\\pythonTesting\\mtcars.xlsx"
+)
 
 r = requests.get("https://coreyms.com")
 
